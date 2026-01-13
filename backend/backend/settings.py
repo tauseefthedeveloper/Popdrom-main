2f1ff549e5473ce7c22885cda552c800e2a8afe7
Tauseef Khan <tauseefbhai85@example.com>
2026-01-13 12:43:27 +0530

    Last commit for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,9 +65,11 @@ MIDDLEWARE = [
 ]
 
 # CORS SETTINGS (FIXED)
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
-CORS_ALLOWED_ORIGINS =True
+CORS_ALLOWED_ORIGINS =[
+    "https://popdrop-frontend.onrender.com",
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
